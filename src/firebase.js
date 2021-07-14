9163b7a2600de7fc40bb601dece7193ca961fc23
lokesh-0706 <lokesh.karri2002@gmail.com>
2021-07-14 17:34:59 +0530

    demo

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlWL0Jy4OqIc4SqTS0vH1MbvBUm058A74",
+    authDomain: "netflix-clone-a62f4.firebaseapp.com",
+    projectId: "netflix-clone-a62f4",
+    storageBucket: "netflix-clone-a62f4.appspot.com",
+    messagingSenderId: "813411301414",
+    appId: "1:813411301414:web:14afb55b817f646b25cee7"
+  };
