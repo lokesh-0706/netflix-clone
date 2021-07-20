0c3e571556c51745a7f1b8a600a83340c67ab8e6
lokesh-0706 <70812570+lokesh-0706@users.noreply.github.com>
2021-07-21 02:24:16 +0530

    final build

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import "./LoginScreen.css"
+import SignupScreen from './SignupScreen';
 
 function LoginScreen() {
     const [signIn,setSignIn] = useState(false);
@@ -15,11 +16,13 @@ function LoginScreen() {
             <div className="loginScreen__gradient">    
             </div>
             <div className="loginScreen__body">
+                {signIn ? (
+                    <SignupScreen />
+                ):
                 <>
                     <h1>Unlimited films, TV programmes and more.</h1>
                     <h2>Watch anywhere, Cancel at any time</h2>
                     <h3>Ready to watch? Enter your Email to create or restart your membership</h3>
-                </>
                 <div className="loginScreen__input">
                     <form>
                         <input type='email' 
@@ -28,6 +31,9 @@ function LoginScreen() {
                         <button onClick={()=> setSignIn(true)} className='loginScreen__getStarted'>Get Started</button>
                     </form>
                 </div>
+                </>                 
+                }
+               
             </div>
         </div>
     )
